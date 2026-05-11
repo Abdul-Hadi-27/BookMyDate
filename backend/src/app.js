e09bf81a3c3d556bc03ff9e92e2134d33fb859da
Abdul Hadi <abdulhadi272003@gmail.com>
2026-05-11 20:26:08 +0530

    m

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,10 +20,12 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://book-my-date.vercel.app",
+    credentials: true,
+  })
+);
 
 
 // Auth Routes
