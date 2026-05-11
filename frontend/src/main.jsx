a1b0c9ae128b190a7c32053de8e3d9f43bc30587
Abdul Hadi <abdulhadi272003@gmail.com>
2026-05-11 19:54:03 +0530

    Your commit message

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,32 +1,30 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import { Provider } from 'react-redux'
-import { store } from './stores/store.js'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { store } from "./stores/store.js";
 
-createRoot(document.getElementById('root')).render(
-
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <Provider store={store}>
+    <Provider store={store}>
+      <App />
+    </Provider>
 
-    <App />
-  </Provider>
     <ToastContainer
-  position="top-center"
-  autoClose={5000}
-  hideProgressBar={false}
-  closeOnClick
-  pauseOnHover={false}
-  draggable
-  theme="dark"
-   toastClassName={() =>
-    "bg-black text-white rounded shadow-lg px-4 py-3"
-  }
-  bodyClassName={() => "text-white"}
-  progressClassName="bg-red-500"
-/>
-
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      closeOnClick
+      pauseOnHover={false}
+      draggable
+      theme="dark"
+      toastClassName={() =>
+        "bg-black text-white rounded shadow-lg px-4 py-3"
+      }
+      bodyClassName={() => "text-white"}
+      progressClassName="bg-red-500"
+    />
   </BrowserRouter>
-)
+);
--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -1,18 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import UserSlice from './reducers/UserSlice'
-import MovieSlice from './reducers/MovieSlice'
-import UiSlice from './reducers/Ui.slice'
-import ShowSlice from './reducers/Showslice'
-import BookingSlice from './reducers/BookingSlice'
 
- export const store=configureStore({
-    reducer:{
+import UserSlice from "./reducers/UserSlice";
+import MovieSlice from "./reducers/MovieSlice";
+import UiSlice from "./reducers/Ui.slice";
+import ShowSlice from "./reducers/Showslice";
+import BookingSlice from "./reducers/BookingSlice";
 
-        userReducer:UserSlice,
-        movieReducer:MovieSlice,
-        uiReducer:UiSlice,
-        showReducer:ShowSlice,
-        bookingReducer:BookingSlice
-
-    }
-})
+export const store = configureStore({
+  reducer: {
+    userReducer: UserSlice,
+    movieReducer: MovieSlice,
+    uiReducer: UiSlice,
+    showReducer: ShowSlice,
+    bookingReducer: BookingSlice,
+  },
+});
