d881d7639b2b1f2abc12aa53b5ee2fbd0073a7ee
Abdul Hadi <abdulhadi272003@gmail.com>
2026-05-11 19:14:15 +0530

    updated project

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import UserSlice from '../stores/reducers/UserSlice'
-import MovieSlice from '../stores/reducers/MovieSlice'
-import UiSlice from '../stores/reducers/Ui.slice'
-import ShowSlice from '../stores/reducers/Showslice'
+import UserSlice from './reducers/UserSlice'
+import MovieSlice from './reducers/MovieSlice'
+import UiSlice from './reducers/Ui.slice'
+import ShowSlice from './reducers/Showslice'
 import BookingSlice from './reducers/BookingSlice'
 
  export const store=configureStore({
